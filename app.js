438007e15cb884b8b27c36e428d055561ccddad3
yashPundhir <yash.pundhir.prof@gmail.com>
2022-11-15 23:11:08 +0530

    UI Part is done

---
+++ app.js
@@ -0,0 +1,12 @@
+const container = "";
+let userDate = document.querySelector("dob").value;
+document.querySelector("#years").textContent = userDate;
+// const error = document.querySelector(".error").textContent;
+let todayDate = new Date();
+console.log(todayDate.getDate());
+
+function getAge() {}
+
+function calcAge() {}
+
+function displayAge() {}
